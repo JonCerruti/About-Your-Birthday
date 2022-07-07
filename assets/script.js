6a7a6f4a5bb1c15e9b23fb60710b62c1e6e9706e
unknown <joseph.jermaine@yahoo.com>
2022-07-07 19:07:45 -0400

    fix local storage

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
 var searchFormEl = document.querySelector('#search-form');
 
-function handleSearchFormSubmit(event) {
+
+var birthday;
+
+if(!localStorage.getItem('dates')) {
+console.log('dates not present setting value')
+ localStorage.setItem('dates', JSON.stringify([{year: '2022', subject: 'sunrise',  readMore:'' }]));
+ birthday = localStorage.getItem("dates")
+}else{
+  console.log("retrieving variables");
+  birthday = localStorage.getItem("dates");
+  }
+  console.log(JSON.parse(birthday));
+
+
+function handleSearchFormSubmit(event) { 
   event.preventDefault();
 
   var searchInputVal = document.querySelector('#search-input').value;
   var formatInputVal = document.querySelector('#format-input').value;
-
+console.log('searchInputVal', searchInputVal);
   if (!searchInputVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  var queryString = './birthday-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
 
   location.assign(queryString);
 }
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+ searchFormEl.addEventListener('submit', handleSearchFormSubmit);
